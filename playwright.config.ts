fc6d8ff1c76fa321140bdb3d49f4f1ac2a6cfc32
AiMlxV <41025@chs.ac.th>
2026-04-26 01:29:56 +0700

    test : add epic 1 US1-1 to 1-4

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,9 +19,13 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
+    command: "npm run build && npm run start",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
+    env: {
+      ...process.env,
+      NEXTAUTH_URL: "http://127.0.0.1:3000",
+    },
+    timeout: 300 * 1000,
   },
 });
